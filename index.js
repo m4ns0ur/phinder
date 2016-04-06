7fb1fcf3b21f17695d0cb8a26af92f0a04aa4c1e
Mansour Rahimi <mnr@mnr-debian>
2016-04-06 12:12:51 +0200

    - move the request options outside the search method - trim the api.key, and cse.cx, because of newline at the end

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ var request = require('request');
 var fs = require('fs');
 var path = require('path');
 
+var requestOptions = {
+	url: 'https://www.googleapis.com/customsearch/v1',
+	agentOptions: {
+		key: fs.readFileSync(path.resolve(__dirname, 'ssl/server.key')),
+		cert: fs.readFileSync(path.resolve(__dirname, 'ssl/server.crt'))
+	},
+	qs: {
+		q: '',
+		num: 1, // 1 to 10
+		start: 1, // 1 to 101
+		imgSize: 'medium',
+		searchType: 'image',
+		//fileType: 'jpg',
+		key: fs.readFileSync(path.resolve(__dirname, 'config/api.key'), 'utf-8').trim(),
+		cx: fs.readFileSync(path.resolve(__dirname, 'config/cse.cx'), 'utf-8').trim()
+	}
+};
+
 var bot = new builder.BotConnectorBot({
 	appId: 'YourAppId', appSecret: 'YourAppSecret'
 });
@@ -14,30 +32,14 @@ bot.add('/', function (session) {
 function searchImage(session) {
 	var query = session.message.text;
 	console.log('query: ', query);
-	var options = {
-		url: 'https://www.googleapis.com/customsearch/v1',
-		agentOptions: {
-			key: fs.readFileSync(path.resolve(__dirname, 'ssl/server.key')),
-			cert: fs.readFileSync(path.resolve(__dirname, 'ssl/server.crt'))
-    },
-		qs: {
-			q: query,
-			num: 1, // 1 to 10
-			start: 1, // 1 to 101
-			imgSize: 'medium',
-			searchType: 'image',
-			//fileType: 'jpg',
-			key: fs.readFileSync(path.resolve(__dirname, 'config/api.key')),
-			cx: fs.readFileSync(path.resolve(__dirname, 'config/cse.cx'))
-		}
-	};
-	request.get(options, function (err, response) {
+	requestOptions.qs.q = query;
+	request.get(requestOptions, function (err, response) {
 		if (err) {
 			console.log(err);
 			session.send('sorry, cannot find anything for now');
 		}
 		var items = JSON.parse(response.body).items;
-		if (items.length < 1) {
+		if (!items || items.length < 1) {
 			session.send('sorry, cannot find an image');
 		}
 		var title = items[0].title;
